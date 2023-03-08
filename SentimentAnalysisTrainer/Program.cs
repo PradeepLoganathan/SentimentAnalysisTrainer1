efc0f9eb1d089932b3a562766666d874841f5982
Pradeep Loganathan <pradeep.loganathan@hotmail.com>
2023-03-08 17:14:21 +1000

    Initial commit

---
+++ SentimentAnalysisTrainer/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using SentimentAnalysisConsoleApp.DataStructures;
+using Common;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Transforms.Text;
+
+namespace SentimentAnalysisConsoleApp
+{
+    internal static class Program
+    {
+        private static readonly string BaseDatasetsRelativePath = @"../../../../Data";
+        private static readonly string DataRelativePath = $"{BaseDatasetsRelativePath}/wikiDetoxAnnotated40kRows.tsv";
+
+        private static readonly string DataPath = GetAbsolutePath(DataRelativePath);
+
+        private static readonly string BaseModelsRelativePath = @"../../../../MLModels";
+        private static readonly string ModelRelativePath = $"{BaseModelsRelativePath}/SentimentModel.zip";
+
+        private static readonly string ModelPath = GetAbsolutePath(ModelRelativePath);
+
+        static void Main(string[] args)
+        {
+            // Create MLContext 
+            // Set a random seed for repeatable/deterministic results across multiple trainings.
+            var mlContext = new MLContext(seed: 1);
+
+            // Load data
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentIssue>(DataPath, hasHeader: true);
+
+            TrainTestData trainTestSplit = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            IDataView trainingData = trainTestSplit.TrainSet;
+            IDataView testData = trainTestSplit.TestSet;
+
+            // Data preparation         
+            var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", new TextFeaturizingEstimator.Options 
+                                        {
+                                            CaseMode = TextNormalizingEstimator.CaseMode.Lower,
+										    //NOTE: not exportable to ONNX
+										    //KeepNumbers = false,
+										    //KeepPunctuations = false,
+										    KeepDiacritics = true
+                                        }, nameof(SentimentIssue.Text));
+
+            // Select algorithm and configure model builder                            
+            var trainer = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features");
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+            // Train the model
+            ITransformer trainedModel = trainingPipeline.Fit(trainingData);
+
+            // Evaluate the model
+            var predictions = trainedModel.Transform(testData);
+            var metrics = mlContext.BinaryClassification.Evaluate(data: predictions, labelColumnName: "Label", scoreColumnName: "Score");
+
+            // Display model accuracy stats
+            ConsoleHelper.PrintBinaryClassificationMetrics(trainer.ToString(), metrics);
+
+            // Persist the trained model to a .ZIP file
+            mlContext.Model.Save(trainedModel, trainingData.Schema, ModelPath);
+            
+           
+
+            using (var filestream = File.Open("mymodel.onnx", FileMode.OpenOrCreate))
+            {
+                mlContext.Model.ConvertToOnnx(trainedModel, trainingData, filestream);
+                Console.Write( "onnx model generated");
+            }
+
+            Console.WriteLine("The model is saved to {0}", ModelPath);
+
+            // TRY IT: Make a single test prediction, loading the model from .ZIP file
+            SentimentIssue sampleStatement = new SentimentIssue { Text = "machine learning is amazing!" };
+
+            // Create prediction engine related to the loaded trained model
+            var predEngine = mlContext.Model.CreatePredictionEngine<SentimentIssue, SentimentPrediction>(trainedModel);
+
+            // Score
+            var resultprediction = predEngine.Predict(sampleStatement);
+
+            Console.WriteLine($"=============== Single Prediction  ===============");
+            Console.WriteLine($"Text: {sampleStatement.Text} | Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Toxic" : "Non Toxic")} sentiment | Probability of being toxic: {resultprediction.Probability} ");
+            Console.WriteLine($"================End of Process.Hit any key to exit==================================");
+            Console.ReadLine();
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath , relativePath);
+
+            return fullPath;
+        }
+    }
+}
